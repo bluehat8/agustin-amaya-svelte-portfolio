b452674e52eef7aa64c5c91c584a9a23046b2159
Agustín Amaya <80552327+bluehat8@users.noreply.github.com>
2025-09-24 20:34:02 -0600

    fix: cv-url

--- src/scripts/urls.ts
+++ src/scripts/urls.ts
@@ -2,6 +2,6 @@ export const profileUrl = 'https://firebasestorage.googleapis.com/v0/b/tiendaskd
 export const githubUrl = 'https://github.com/bluehat8'
 export const linkedinUrl = 'https://www.linkedin.com/in/agust%C3%ADn-amaya-b3b110244/'
 export const instagramUrl = 'https://www.instagram.com/agustinmy_/'
-export const cvUrl = 'https://firebasestorage.googleapis.com/v0/b/tiendaskd-779b1.appspot.com/o/CV-AGUSTIN.pdf?alt=media&token=da63ea66-fed0-406d-9fa3-48cf7e2e8afa';
+export const cvUrl = 'https://mega.nz/file/U9hwgR7a#N-q6maSRz69j_jxV7H0zejvOEV-CaWvHoUJcQiD-eaM';
 export const ethVolcanoUrl = 'https://firebasestorage.googleapis.com/v0/b/agustin-52df7.appspot.com/o/ETH-volcano-cuchulink.png?alt=media&token=130d90e7-94a7-4483-b4e8-2ad538de42f3'
 export const neoarkUrl = '/images/neoark.png'
